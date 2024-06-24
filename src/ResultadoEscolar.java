60d159ae3763819c21cbe7d4829000b7940a469c
Rael-br <rael.souzachacon@gmail.com>
2024-06-24 15:41:06 -0300

    24/06/2024

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,11 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int note = 6;
+        if (note >= 7) 
+            System.out.println("Aprovado");
+
+        else
+            System.out.println("Reprovado");
+        
+    }
+}
