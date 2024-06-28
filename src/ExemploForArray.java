a37e40e4c3dbfeae1078a4e5a9cf11b6d941d7f0
Rael-br <rael.souzachacon@gmail.com>
2024-06-28 17:14:58 -0300

    28/06/2024

---
+++ src/ExemploForArray.java
@@ -0,0 +1,12 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        // em arrays o indice começa no zero
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+        
+    
+    }   
+    }
